34da9320f378f220505e2849af4e90fd3f3c701a
lyg <2100761313@qq.com>
2018-12-25 14:20:39 +0800

    输入了system.out.println("你扯淡");

--- src/main/java/com/guoyun/demo2/controller/HelloController.java
+++ src/main/java/com/guoyun/demo2/controller/HelloController.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Controller;
 public class HelloController {
     public void function1(){
         System.out.println("hahah");
+        System.out.println("你扯淡");
     }
 }
